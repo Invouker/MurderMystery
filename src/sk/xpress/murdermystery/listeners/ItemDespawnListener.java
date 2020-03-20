1e6a5bdb4c99535e02d8cec51f0afe8ae79a880d
12XpresS12 <12XpresS12@gmail.com>
2020-03-20 18:47:18 +0100

    Bugfix with despawning items
    Problem with despawning item like bow and gold ingot

--- src/sk/xpress/murdermystery/listeners/ItemDespawnListener.java
+++ src/sk/xpress/murdermystery/listeners/ItemDespawnListener.java
@@ -8,7 +8,12 @@ public class ItemDespawnListener implements Listener {
 	
 	@EventHandler
 	public void onItemDespawnEvent(ItemDespawnEvent  e) {
-		
+		switch(e.getEntity().getItemStack().getType()) {
+			case BOW: 
+			case GOLD_INGOT:
+				e.setCancelled(true);
+		default: break;
+		}
 	}
 
 }
